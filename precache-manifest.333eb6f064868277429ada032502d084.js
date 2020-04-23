a06856bc864bf04c0466a1117c3f645f0811a577
vadimLuzyanin <luzianin999@inbox.ru>
2020-04-23 12:44:38 +0300

    Updates

---
+++ precache-manifest.333eb6f064868277429ada032502d084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c93ba2443e68093d08011a5a3abb3e",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "87f8d194d862f4cc00a1",
+    "url": "/React-Calculator/static/js/2.b206a444.chunk.js"
+  },
+  {
+    "revision": "e53763128d977c2aa839cd529c578e9f",
+    "url": "/React-Calculator/static/js/2.b206a444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49566ff0930057cab3a0",
+    "url": "/React-Calculator/static/js/main.6aa9f708.chunk.js"
+  },
+  {
+    "revision": "15c91afa8704d5f3797f",
+    "url": "/React-Calculator/static/js/runtime-main.efd3fa2b.js"
+  }
+]);
